cce2db97a2b7e420743ff351c683bdeb26c5cc0c
Bewin007 <bewinfelix@karunya.edu.in>
2023-10-26 11:37:43 +0530

    a

--- Web Tech/ex10/Node/MyApp.js
+++ Web Tech/ex10/Node/MyApp.js
@@ -1,17 +1,59 @@
-const http = require('http')
-const fs = require('fs')
+const http = require('http');
+const fs = require('fs');
+const mongoose = require('mongoose');
+const { URLSearchParams } = require('url'); // Required for parsing form data
 
-const server = http.createServer(function(req,res){
+mongoose.connect('mongodb://127.0.0.1:27017/UsersDB').then(function(){
+    console.log('Database connected');
+});
 
-    if (req.url == '/'){
-        res.writeHead(200,{'Content-Type': 'text/html'})
-        fs.createReadStream('app.html').pipe(res)
-        
+const userschema = new mongoose.Schema({
+    Name: String,
+    Password: String,
+    Age: Number,
+    MobileNumber: Number,
+    Email: String,
+    Gender: String,
+    State: String
+});
 
+const usermodel = mongoose.model('Users', userschema);
 
+const server = http.createServer(function(req, res) {
+    if (req.url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        fs.createReadStream('home.html').pipe(res); // Serve the HTML form
+    } else if (req.url === '/save' && req.method === 'POST') {
+        let rawdata = '';
+        req.on('data', function(data) {
+            rawdata += data;
+        });
+
+        req.on('end', function() {
+            let formdata = new URLSearchParams(rawdata);
+            usermodel.create({
+                Name: formdata.get('name'),
+                Password: formdata.get('pass'),
+                Age: formdata.get('age'),
+                MobileNumber: formdata.get('num'),
+                Email: formdata.get('email'),
+                Gender: formdata.get('gender'),
+                State: formdata.get('state')
+            }, function(err, user) {
+                if (err) {
+                    res.writeHead(500, { 'Content-Type': 'text/html' });
+                    res.write('Error saving data');
+                    res.end();
+                } else {
+                    res.writeHead(200, { 'Content-Type': 'text/html' });
+                    res.write('Data Saved Successfully');
+                    res.end();
+                }
+            });
+        });
     }
-})
+});
 
-server.listen('3000',function(){
-    console.log('server stated')
-})
+server.listen(8000, function() {
+    console.log('Server started');
+});
