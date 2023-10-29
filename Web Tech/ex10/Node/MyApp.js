3102a3b4be1ef7cfd3548f7f2eebe89b9b9a517b
Bewin007 <bewinfelix@karunya.edu.in>
2023-10-29 21:42:31 +0530

    a

--- Web Tech/ex10/Node/MyApp.js
+++ Web Tech/ex10/Node/MyApp.js
@@ -1,59 +1,63 @@
 const http = require('http');
 const fs = require('fs');
+
+
 const mongoose = require('mongoose');
-const { URLSearchParams } = require('url'); // Required for parsing form data
+mongoose.connect('mongodb://127.0.0.1:27017/ecommerce')
+        .then(function(){
+            console.log('DB Connected')
+        })
+const customerSchema = new mongoose.Schema({name:String,password:String,age:String,mobilenumber:String,email:String,gender:String,state:String});
+const customermodel = mongoose.model('customers',customerSchema)
 
-mongoose.connect('mongodb://127.0.0.1:27017/UsersDB').then(function(){
-    console.log('Database connected');
-});
 
-const userschema = new mongoose.Schema({
-    Name: String,
-    Password: String,
-    Age: Number,
-    MobileNumber: Number,
-    Email: String,
-    Gender: String,
-    State: String
-});
-
-const usermodel = mongoose.model('Users', userschema);
-
-const server = http.createServer(function(req, res) {
-    if (req.url === '/') {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        fs.createReadStream('home.html').pipe(res); // Serve the HTML form
-    } else if (req.url === '/save' && req.method === 'POST') {
-        let rawdata = '';
-        req.on('data', function(data) {
-            rawdata += data;
-        });
-
-        req.on('end', function() {
-            let formdata = new URLSearchParams(rawdata);
-            usermodel.create({
-                Name: formdata.get('name'),
-                Password: formdata.get('pass'),
-                Age: formdata.get('age'),
-                MobileNumber: formdata.get('num'),
-                Email: formdata.get('email'),
-                Gender: formdata.get('gender'),
-                State: formdata.get('state')
-            }, function(err, user) {
-                if (err) {
-                    res.writeHead(500, { 'Content-Type': 'text/html' });
-                    res.write('Error saving data');
-                    res.end();
-                } else {
-                    res.writeHead(200, { 'Content-Type': 'text/html' });
-                    res.write('Data Saved Successfully');
-                    res.end();
-                }
-            });
-        });
+const server = http.createServer(function(req,res){
+    if(req.url === '/'){
+        res.writeHead('200',{'Content-Type':'text/html'});
+        fs.createReadStream('home.html').pipe(res);
     }
-});
+    else if(req.url === '/register' && req.method === 'POST'){
+        var rawdata = '';
+        req.on('data',function(data){
+            rawdata += data;
+        })
+        req.on('end',function(){
+            var formdata = new URLSearchParams(rawdata);
+            res.writeHead('200',{'Content-Type':'text/html'});
+            customermodel.create({name:formdata.get('name'),
+                                  password:formdata.get('password'),
+                                  age:formdata.get('age'),
+                                  mobilenumber:formdata.get('mobilenumber'),
+                                  email:formdata.get('email'),
+                                  gender:formdata.get('gender'),
+                                  state:formdata.get('state')
 
-server.listen(8000, function() {
-    console.log('Server started');
-});
+        })
+        res.write('Data Saved Successfully');
+        res.end();
+        })
+    }
+    else if(req.url === '/view' && req.method === "GET"){
+        res.writeHead('200',{'Content-Type':'text/html'});
+        customermodel.find().then(function(customers){
+            res.write("List Customer Details");
+            res.write("<table border=1 cellspacing=0 width=400>");
+            res.write("<tr><th>Name</th><th>Password</th><th>Age</th><th>Mobile Number</th><th>Email</th><th>Gender</th><th>State</th>");
+            customers.forEach(customers=>{
+                res.write("<tr>");
+                res.write("<td>"+customers.name+"</td>");
+                res.write("<td>"+customers.password+"</td>");
+                res.write("<td>"+customers.age+"</td>");
+                res.write("<td>"+customers.mobilenumber+"</td>");
+                res.write("<td>"+customers.email+"</td>");
+                res.write("<td>"+customers.gender+"</td>");
+                res.write("<td>"+customers.state+"</td>");
+                res.write("</tr>");
+            })
+            res.end();
+        })
+    }
+})
+server.listen('8000',function(){
+    console.log('Server started at port http://127.0.0.1:8000')
+})
